08727b8e626679a2f807b13d975e33502936b75a
ase10git <ase1031@naver.com>
2025-04-23 10:22:24 +0900

    refactor : Home 페이지 HTML 선택자 수정

--- homeComponents/js/insertVideoContents.js
+++ homeComponents/js/insertVideoContents.js
@@ -28,14 +28,14 @@ fetch("./homeComponents/html/videoContent.html")
 
         // 메뉴 아이템 태그
         const menu_template = temp_div.querySelector("#menu-template").content;
-        let menu_items = temp_div.querySelector("#content-template").content.querySelector("#menu-items");
+        let menu_items = temp_div.querySelector("#content-template").content.querySelector(".menu-items");
 
         menu_titles.forEach(el => {
             // 템플릿 내용 복제
             const clone = menu_template.cloneNode(true);
             // 요소 수정
-            clone.querySelector("#menu-icon-img").src = public_url + el.img_name;
-            clone.querySelector("#menu-title").textContent = el.title;
+            clone.querySelector(".menu-icon-img").src = public_url + el.img_name;
+            clone.querySelector(".menu-name").textContent = el.title;
 
             // 추가
             menu_items.appendChild(clone);
@@ -50,11 +50,11 @@ fetch("./homeComponents/html/videoContent.html")
             const clone = content_template.cloneNode(true);
 
             // 요소 수정
-            clone.querySelector("#thumbnail-img").src = el.thumbnail;
-            clone.querySelector("#video-title").textContent = el.title;
-            clone.querySelector("#channel-name").textContent = el.uploader;
-            clone.querySelector("#spectator-number").textContent = `조회수 ${el.spectators}`;
-            clone.querySelector("#upload-time").textContent = el.uploaded_time;
+            clone.querySelector(".thumbnail-img").src = el.thumbnail;
+            clone.querySelector(".video-title").textContent = el.title;
+            clone.querySelector(".channel-name").textContent = el.uploader;
+            clone.querySelector(".spectator-number").textContent = `조회수 ${el.spectators}`;
+            clone.querySelector(".upload-time").textContent = el.uploaded_time;
 
             // 추가
             contents.appendChild(clone);
