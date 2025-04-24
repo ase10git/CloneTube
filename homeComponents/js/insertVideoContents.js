9a06e6c14278ba49a1aceabd504f640bae01b679
hyojin <hyojjjinn@gmail.com>
2025-04-24 13:00:36 +0900

    Fix home.css/ html,js

--- homeComponents/js/insertVideoContents.js
+++ homeComponents/js/insertVideoContents.js
@@ -4,14 +4,28 @@ import menu_titles from "./videoMenuList.js";
 // 이미지 경로
 const public_url = '../../images/';
 
-// 비디오 정보 --- 테스트용
-const video = {title: 'test', uploader: 'aaa', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '#'};
-const video_info = Array.from(
-    {length: 16},
-    (val) => {return val = video}
-)
+// 비디오 정보
+const video = [ 
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alan Cooper', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/alan.svg'},    
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Marcus Levin', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/marcus.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alexis Sears', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/alexis.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Jesica Lambert', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/jesica.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Anna White', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/anna.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Skylar Dias', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/skylar.svg'},    
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/james.svg'},
+    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2002-03-03', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
+]
 
-// videoContent.html을 가져옴
+const video_info = video;
+
 const temp_div = document.createElement("div");
 
 // 템플릿 코드를 사용하여 비디오 컨텐츠 생성
@@ -31,13 +45,11 @@ fetch("./homeComponents/html/videoContent.html")
         let menu_items = temp_div.querySelector("#content-template").content.querySelector(".menu-items");
 
         menu_titles.forEach(el => {
-            // 템플릿 내용 복제
+
             const clone = menu_template.cloneNode(true);
             // 요소 수정
             clone.querySelector(".menu-icon-img").src = public_url + el.img_name;
             clone.querySelector(".menu-name").textContent = el.title;
-
-            // 추가
             menu_items.appendChild(clone);
         });
 
@@ -46,17 +58,15 @@ fetch("./homeComponents/html/videoContent.html")
         const contents = document.querySelector("#contents");
 
         video_info.forEach(el => {
-            // 템플릿 내용 복제
+
             const clone = content_template.cloneNode(true);
 
-            // 요소 수정
             clone.querySelector(".thumbnail-img").src = el.thumbnail;
             clone.querySelector(".video-title").textContent = el.title;
             clone.querySelector(".channel-name").textContent = el.uploader;
             clone.querySelector(".spectator-number").textContent = `조회수 ${el.spectators}`;
             clone.querySelector(".upload-time").textContent = el.uploaded_time;
-
-            // 추가
+            clone.querySelector(".avatar-img").src = el.avatar_img;
             contents.appendChild(clone);
         })
     })
