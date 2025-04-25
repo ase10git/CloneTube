d2b059f187399f3d2e71d61266e5125d348418be
ase10git <ase1031@naver.com>
2025-04-25 13:14:21 +0900

    refactor : 홈, 채널 페이지 CSS 영역 수정
    nav-bar JS 이벤트 수정

--- js/topheader/topheader.js
+++ js/topheader/topheader.js
@@ -1,14 +1,21 @@
+// side nav의 버튼 동작을 제어
 document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.querySelector(".side-bar");
-    const toggleButton = document.getElementById("SideButton");
+    // nav 요소가 로딩될 때까지 탐색 
+    const interval = setInterval(() => {
+        // console.log('nav 요소 탐색중..')
+        
+        // 이벤트를 넣을 대상 요소 선택
+        const sidebar = document.querySelector("#side-bar");
+        const toggleButton = document.querySelector("#side-button");
 
-    // 디버깅을 위한 콘솔 로그 추가
-    console.log('sidebar:', sidebar);
-    console.log('toggleButton:', toggleButton);
-
-    if (sidebar && toggleButton) {
-        toggleButton.addEventListener("click", function () {
-            sidebar.classList.toggle("active");
-        });
-    } 
+        // console.log(sidebar, toggleButton);
+        
+        // 요소가 있을 때 이벤트 추가
+        if (sidebar && toggleButton) {
+            toggleButton.addEventListener("click", function () {
+                sidebar.classList.toggle("active");
+            });
+            clearInterval(interval); // 이벤트 연결 후 멈춤
+        }
+    }, 100);
 });
