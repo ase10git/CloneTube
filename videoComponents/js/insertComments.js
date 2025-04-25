a88a3dfa8170b544c2c44a25ac01f76b0f16f230
ase10git <ase1031@naver.com>
2025-04-25 19:11:44 +0900

    feat(video) : HTML 구조 추가
    기본 구조 추가(동영상, 동영상 정보, 댓글)

---
+++ videoComponents/js/insertComments.js
@@ -0,0 +1,45 @@
+// ------ 비디오 페이지의 댓글 추가 ------
+// 테스트용 댓글
+const comments = [
+    {author: 'kim', profile: '', commented_at: '2024.10.24', body: '댓글 추가했습니다', liked: 100, disliked: 1},
+    {author: 'park', profile: '', commented_at: '2023.04.17', body: '영상 잘 봤습니다', liked: 1, disliked: 0},
+    {author: 'test', profile: '', commented_at: '2023.12.10', body: '댓글 내용 테스트중', liked: 30, disliked: 0},
+    {author: 'oliver', profile: '', commented_at: '2024.02.15', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio. Etiam vel consectetur lacus, vel efficitur sem. Suspendisse vestibulum libero sodales velit facilisis, at maximus ligula dignissim. Cras dui augue, dignissim a ullamcorper ut, condimentum eu enim. Quisque eleifend efficitur feugiat. Integer consequat vestibulum risus, sed ornare mi tincidunt non. Nunc malesuada rhoncus diam, vel cursus odio euismod nec. Donec ac laoreet lectus.', liked: 100, disliked: 10},
+    {author: 'loopin', profile: '', commented_at: '2024.09.01', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio.', liked: 100, disliked: 1},
+]
+
+// 이미지 경로
+const public_url = '../../images/';
+
+const temp_div = document.createElement("div");
+
+// 템플릿 코드를 사용하여 비디오 컨텐츠 생성
+fetch("../videoComponents/html/commentTemplate.html")
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("HTML template 불러오기 실패");
+        }
+        return res.text();
+    })
+    .then(data => {
+        // 문자열로 로드된 HTML을 DOM으로 파싱
+        temp_div.innerHTML = data;
+
+        // 댓글 템플릿 태그
+        const comment_template = temp_div.querySelector("#comment-template").content;
+
+        // 댓글 위치
+        const contents = document.querySelector("#comments-list");
+
+        comments.forEach(el => {
+
+            const clone = comment_template.cloneNode(true);
+
+            clone.querySelector(".commentor-author-profile").src = el.profile;
+            clone.querySelector(".comment-author").textContent = el.author;
+            clone.querySelector(".comment-uploaded").textContent = el.commented_at;
+            clone.querySelector(".comment-body").textContent = el.body;
+            clone.querySelector(".comment-liked-number").textContent = el.liked;
+            contents.appendChild(clone);
+        })
+    })
---
+++ videoComponents/js/insertVideoFeedbackMenu.js
@@ -0,0 +1,92 @@
+// ------ 동영상의 메뉴 리스트(좋아요, 공유 등) 생성 ------
+
+// 상시 표시용 메뉴 목록
+const feedback_list = [
+    {id: "like", name: "좋아요", img_src: "liked-videos.svg"},
+    {id: "dislike", name: "싫어요", img_src: "/DisLiked.svg"},
+]
+
+// 반응형에 따라 다르게 표시되는 메뉴 목록
+const feedback_list_dynamic = [
+    {id: "share", name: "공유", img_src: "share_arrow.svg"},
+    {id: "save", name: "저장", img_src: "list_save.svg"},
+    {id: "clip", name: "클립", img_src: "scissors.svg"},
+]
+
+// 항상 드랍메뉴에 있는 메뉴 목록
+const feedback_list_hidden = [
+    {id: "download", name: "오프라인 저장", img_src: "download.svg"},
+    {id: "report", name: "신고", img_src: "report-history.svg"},
+]
+
+// 비디오 메뉴 태그
+const video_menu = document.querySelector("#video-menu");
+
+// 메뉴 버튼 추가 함수
+function append_element(element, parentElement) {
+    // 버튼 태그 생성
+    const item = document.createElement("button");
+
+    // 메뉴 버튼 양식
+    let menu_item_html = 
+    `<div class="menu-icon-box">
+        <img src="../images/${element.img_src}" alt="${element.name}" class="icon-img">
+    </div>
+    <div class="menu-option-name" id="menu-${element.id}">
+        ${(element.id === "like" || element.id === "dislike") ? "" : element.name}
+    </div>`;
+
+    // 버튼을 부모 요소에 추가
+    item.innerHTML = menu_item_html;
+    item.classList.add("video-menu-btn");
+
+    parentElement.appendChild(item);
+}
+
+// 더보기 버튼 추가 함수
+function append_show_more_btn() {
+    // 버튼 태그 생성 및 클래스와 id 지정
+    const show_more_btn = document.createElement("button");
+    show_more_btn.id = "menu-show-more";
+    show_more_btn.classList.add("video-menu-btn");
+    
+    // 더보기 버튼 양식
+    show_more_btn.innerHTML = 
+    `<div class="menu-icon-box">
+        <img src="../images/three-dots.svg" alt="show-more-three-dots" class="icon-img">
+    </div>`;
+
+    // 버튼을 부모 요소에 추가
+    video_menu.appendChild(show_more_btn);
+}
+
+// 비디오 메뉴 버튼을 생성하여 HTML에 추가
+function build_menu() {
+    // 상시 표시용 메뉴 추가
+    feedback_list.forEach(el=>{
+        append_element(el, video_menu);
+    });
+
+    // 드랍 메뉴 생성
+    const drop_menu = document.createElement("div");
+    drop_menu.id = "video-menu-dropbox";
+
+    // 동적 메뉴 추가
+    feedback_list_dynamic.forEach(el=>{
+        append_element(el, video_menu);
+        append_element(el, drop_menu);
+    })
+
+    // 항상 드랍다운에 있는 메뉴 추가
+    feedback_list_hidden.forEach(el=>{
+        append_element(el, drop_menu);
+    });
+
+    // 더보기 버튼 추가
+    append_show_more_btn();
+
+    // 드랍 메뉴 추가
+    video_menu.appendChild(drop_menu);
+}
+
+build_menu();
