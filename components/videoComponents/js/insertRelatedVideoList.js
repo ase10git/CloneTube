8039d480ff9f383583b1b1871aeeaae56647efdc
SuyunLee <dufrhdsy@naver.com>
2025-04-29 12:56:55 +0900

    add: video 페이지 css 및 js 2차 수정작업

--- components/videoComponents/js/insertRelatedVideoList.js
+++ components/videoComponents/js/insertRelatedVideoList.js
@@ -1,66 +1,100 @@
 // ------ 비디오 페이지의 관련 동영상 추천 목록 추가 ------
 import build_video_menu from "../../videoComponents/js/insertVideoMenu.js";
 
-// 테스트용 데이터
-const video = [ 
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alan Cooper', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/alan.svg'},    
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Marcus Levin', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/marcus.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alexis Sears', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/alexis.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Jesica Lambert', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/jesica.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Anna White', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/anna.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Skylar Dias', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/skylar.svg'},    
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/james.svg'},
-    {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
-]
 
-const public_url = "../../images/";
+// HTTPRequest 객체 생성
+const xhr = new XMLHttpRequest();
 
-const temp_div = document.createElement("div");
+// get 요청 설정 - 비디오 리스트를 비동기로 가져오기
+xhr.open("GET", "http://techfree-oreumi-api.kro.kr:5000/video/getVideoList", true);
 
-// 템플릿 코드를 사용하여 비디오 컨텐츠 생성
-function insert_video_list() {
-    fetch("../components/videoComponents/html/videoTemplate.html")
-    .then(res => {
-        if (!res.ok) {
-            throw new Error("HTML template 불러오기 실패");
+// 요청 전송 후 상태 변화 시 콜백 함수
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        if(xhr.status === 200) {
+            // 결과 데이터 파싱
+            const data = JSON.parse(xhr.response);
+            console.log(data);
+            // 비디오 목록에 데이터 추가
+            video_list(data);
+        } else {
+            // 에러 처리
+            console.error("Error:", xhr.status);
         }
-        return res.text();
-    })
-    .then(data => {
-        // 문자열로 로드된 HTML을 DOM으로 파싱
-        temp_div.innerHTML = data;
+    }
+};
 
-        // 비디오 태그
-        const video_template = temp_div.querySelector("#video-template").content;
-        const recommend_box = document.querySelectorAll(".recommend-box");
+// 요청 전송
+xhr.send();  
 
-        // 비디오 메뉴 가져오기
-        const menu = build_video_menu("../images/");
 
-        video.forEach(el => {
-            const clone = video_template.cloneNode(true);
+function video_list(data){
+    const video = data;
 
-            clone.querySelector(".video-thumbnail-img").src = el.thumbnail;
-            clone.querySelector(".video-title").textContent = el.title;
-            clone.querySelector(".channel-name").textContent = el.uploader;
-            clone.querySelector(".spectator-number").textContent = `조회수 ${el.spectators}`;
-            clone.querySelector(".uploaded-time").textContent = el.uploaded_time;
-            clone.querySelector(".menu-box-img").src = public_url + 'three-dots-vertical.svg';
-            clone.querySelector(".video-menu").innerHTML = menu.outerHTML;
+    const public_url = "../../images/";
 
-            // need fix : 첫 번째에만 들어가는 문제 발생
-            recommend_box[0].appendChild(clone);
-            recommend_box[1].appendChild(clone);
+    const temp_div = document.createElement("div");
+
+    // 템플릿 코드를 사용하여 비디오 컨텐츠 생성
+    function insert_video_list() {
+        fetch("../components/videoComponents/html/videoTemplate.html")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("HTML template 불러오기 실패");
+            }
+            return res.text();
         })
-    })
+        .then(data => {
+            // 문자열로 로드된 HTML을 DOM으로 파싱
+            temp_div.innerHTML = data;
+
+            // 비디오 태그
+            const video_template = temp_div.querySelector("#video-template").content;
+            const recommend_box = document.querySelectorAll(".recommend-box");
+
+            // 비디오 메뉴 가져오기
+            const menu = build_video_menu("../images/");
+
+            video.forEach(el => {
+                const clone = video_template.cloneNode(true);
+
+                clone.querySelector(".video-thumbnail-img").src = el.thumbnail;
+                clone.querySelector(".video-title").textContent = el.title;
+                clone.querySelector(".channel-name").textContent = el.channel_id;
+                clone.querySelector(".spectator-number").textContent = `조회수 ${el.views}`;
+                clone.querySelector(".uploaded-time").textContent = el.created_dt;
+                clone.querySelector(".menu-box-img").src = public_url + 'three-dots-vertical.svg';
+                clone.querySelector(".video-menu").innerHTML = menu.outerHTML;
+
+                recommend_box.forEach(el=>{
+                    el.appendChild(clone.cloneNode(true));
+                });
+            })
+        })
+    }
+    insert_video_list();
 }
 
-insert_video_list();
+
+// 테스트용 데이터
+// const video = [ 
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alan Cooper', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/alan.svg'},    
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Marcus Levin', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/marcus.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Alexis Sears', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/alexis.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Jesica Lambert', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/jesica.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Anna White', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/anna.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'Skylar Dias', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/skylar.svg'},    
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-2.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-1.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-3.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-4.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-5.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-6.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-7.svg', avatar_img: '../images/james.svg'},
+//     {title: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.', uploader: 'James Gouse', spectators: 123, uploaded_time: '2024-01-02', thumbnail: '../images/thumbnail-8.svg', avatar_img: '../images/james.svg'},
+// ]
+
+
+
