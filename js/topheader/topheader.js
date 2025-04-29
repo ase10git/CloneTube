6725a752c56971987350cfd36cc4fb0098d1bd12
ase10git <ase1031@naver.com>
2025-04-29 13:59:12 +0900

    add(topheader) : 검색 폼 추가

--- js/topheader/topheader.js
+++ js/topheader/topheader.js
@@ -1,3 +1,27 @@
+// 이벤트 리스너 등록
+function form_event() {
+    const search_form = document.querySelector("#search-form");
+    const search_input = document.querySelector("#search-input");
+    const reset_button = document.querySelector(".reset-button");
+
+    // 검색 폼에서 유효성 검사
+    search_form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const search_query = search_input.value;
+        if (!search_query) return;
+        window.location.href = `search.html?query=${search_query}`;
+    });
+
+    // input 내의 값 유무에 따른 reset button 표시
+    search_input.addEventListener("input", function () {
+        if (search_input.value) {
+            reset_button.classList.add("visible");
+        } else {
+            reset_button.classList.remove("visible");
+        }
+    });
+}
+
 // side nav의 버튼 동작을 제어
 document.addEventListener("DOMContentLoaded", function () {
     // nav 요소가 로딩될 때까지 탐색 
@@ -14,5 +38,8 @@ document.addEventListener("DOMContentLoaded", function () {
             });
             clearInterval(interval); // 이벤트 연결 후 멈춤
         }
+
+        // form 이벤트 등록
+        form_event();
     }, 100);
 });
