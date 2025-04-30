85c8064dd7aafcced348d48b4b8f62447201e75c
ase10git <ase1031@naver.com>
2025-04-30 18:01:03 +0900

    style(search) : css 4차 수정
    비디오 메뉴 드랍메뉴 스타일 추가, 검색한 비디오들의 채널 정보 가져오는 코드 리팩터링

--- components/searchComponents/js/insertSearchVideoList.js
+++ components/searchComponents/js/insertSearchVideoList.js
@@ -5,6 +5,44 @@ import viewsCalculator from "../../../js/util/viewsCalculator.js";
 
 const public_url = '../../images/';
 
+// 비디오 메뉴 수정
+function edit_menu() {
+    // 비디오 메뉴 가져오기
+    let menu = build_video_menu("../images/");
+
+    // 메뉴 아이템 제거(추천 관련 옵션 2개 제거)
+    menu.removeChild(menu.childNodes[7]);
+    menu.removeChild(menu.childNodes[7]);
+
+    // 구분 바 요소 추가
+    const service_bar = document.createElement("li");
+    service_bar.classList.add("service-bar");
+
+    // 신고 항목 위에 구분바 추가
+    menu.insertBefore(service_bar, menu.childNodes[7]);
+
+    return menu;
+}
+
+// 검색 결과가 없을 때 출력 내용 생성
+function no_result_html(query) {
+    const html_template = 
+    `
+    <div class="no-search-result">
+        <div class="no-search-result-img-box">
+            <img src="../../images/no-search-result.svg" alt="no-search-result">
+        </div>
+        <div>
+            <span class="no-result-message">${query} 검색결과가 없습니다.</span>
+        </div>
+        <div>
+            <span class="no-result-recommend">다른 검색어를 시도해 보거나 검색 필터를 삭제하세요.</span>
+        </div>
+    </div>
+    `
+    return html_template;
+}
+
 // 검색 결과 만들기
 async function insert_search_results(query, total_info) {
     // 템플릿을 담을 태그
@@ -28,18 +66,13 @@ async function insert_search_results(query, total_info) {
         const contents = document.querySelector("#contents");
     
         // 비디오 메뉴 가져오기
-        const menu = build_video_menu("../images/");
+        const video_menu = edit_menu();
 
         // 결과 배열 길이에 따른 화면 처리
         if (total_info.length == 0) {
-            // 결과가 없을 때 처리
-            const result_div = document.createElement("div");
-            result_div.classList.add("search-result");
-            result_div.textContent = `'${query}' 검색 결과가 없습니다.`;
-            contents.appendChild(result_div);
-            return;
+            contents.innerHTML = no_result_html(query);
         } else {
-            
+            // 검색한 비디오 데이터 출력
             // 결과가 있을 때 처리
             total_info.forEach(el => {
                 // 복제할 DOM
@@ -48,7 +81,17 @@ async function insert_search_results(query, total_info) {
                 const uploaded_time = timeCalculator(el.created_dt);
                 const views = viewsCalculator(el.views, "kor");
 
+                // 비디오 링크
+                const video_link = clone.querySelectorAll(".video-link");
+                video_link.forEach(link=>{
+                    link.href = `video.html?video_id=${el.id}`;
+                })
+
+                // 채널 링크
+                clone.querySelector(".channel-link").href = `channel.html?channel_id=${el.channel_id}`;
+
                 // 요소에 넣기
+                clone.querySelector(".thumbnail-img").id = el.id;
                 clone.querySelector(".thumbnail-img").src = el.thumbnail;
                 clone.querySelector(".thumbnail-img").alt = el.title;
                 clone.querySelector(".video-title").textContent = el.title;
@@ -57,16 +100,44 @@ async function insert_search_results(query, total_info) {
                 clone.querySelector(".upload-time").textContent = uploaded_time;
                 clone.querySelector(".avatar-img").src = el.channel_profile;
                 clone.querySelector(".video-description").textContent = el.description;
-                clone.querySelector(".menu-list").innerHTML = menu.outerHTML;
+                clone.querySelector(".menu-list").innerHTML = video_menu.outerHTML;
                 clone.querySelector(".btn-icon").src = public_url + "three-dots-vertical.svg";
                 clone.querySelector(".btn-icon").alt = "dot-three-icon";
+
                 contents.appendChild(clone);
             });
+            
+            // 비디오 메뉴 이벤트 처리
+            const video_menu_all = document.querySelectorAll(".menu");
+            // 메뉴 버튼 클릭 시 리스트 뜨는 이벤트 추가
+            video_menu_all.forEach(el => {
+                const menu_btn = el.querySelector(".menu-toggle-btn");
+                const menu_list = el.querySelector(".menu-list");
+
+                menu_btn.addEventListener("click", () => {
+                    // 클릭 시점에서 버튼의 활성화 여부 판단
+                    const is_active = menu_btn.classList.contains("active");
+
+                    // 버튼 활성화 클래스 제거, 열려있는 메뉴 닫기
+                    video_menu_all.forEach(target => {
+                        target.querySelector(".menu-toggle-btn").classList.remove("active");
+                        target.querySelector(".menu-list").classList.remove("active");
+                    });
+                    
+                    // 버튼이 비활성화였다면 보이도록 표시
+                    // 활성화 여부 active로 재설정
+                    if (!is_active) {
+                        menu_btn.classList.add("active");
+                        menu_list.classList.add("active");
+                    }
+                });
+            })
         }
     })
     .catch(error => {
         console.error("Error:", error);
     });
 }
+
 
 export default insert_search_results;
