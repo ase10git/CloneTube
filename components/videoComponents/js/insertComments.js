ce92de758d625fd21bb1d6083d8a178b8d745a16
SuyunLee <dufrhdsy@naver.com>
2025-05-01 15:47:27 +0900

    add : 비디오 채널 댓글 기능 작업 중 (1)

--- components/videoComponents/js/insertComments.js
+++ components/videoComponents/js/insertComments.js
@@ -1,11 +1,13 @@
+import timeCalculator from "../../../js/util/timeCalculator.js";
+
 // ------ 비디오 페이지의 댓글 추가 ------
 // 테스트용 댓글
 const comments = [
-    {author: 'kim', profile: '../../../images/marcus.svg', commented_at: '2024.10.24', body: '댓글 추가했습니다', liked: 100, disliked: 1},
-    {author: 'park', profile: '../../../images/marcus.svg', commented_at: '2023.04.17', body: '영상 잘 봤습니다', liked: 1, disliked: 0},
-    {author: 'test', profile: '../../../images/marcus.svg', commented_at: '2023.12.10', body: '댓글 내용 테스트중', liked: 30, disliked: 0},
-    {author: 'oliver', profile: '../../../images/marcus.svg', commented_at: '2024.02.15', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio. Etiam vel consectetur lacus, vel efficitur sem. Suspendisse vestibulum libero sodales velit facilisis, at maximus ligula dignissim. Cras dui augue, dignissim a ullamcorper ut, condimentum eu enim. Quisque eleifend efficitur feugiat. Integer consequat vestibulum risus, sed ornare mi tincidunt non. Nunc malesuada rhoncus diam, vel cursus odio euismod nec. Donec ac laoreet lectus.', liked: 100, disliked: 10},
-    {author: 'loopin', profile: '../../../images/marcus.svg', commented_at: '2024.09.01', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio.', liked: 100, disliked: 1},
+    {author: 'kim', profile: '../../../images/marcus.svg', commented_at: '1년 전', body: '댓글 추가했습니다', liked: 100, disliked: 1},
+    {author: 'park', profile: '../../../images/marcus.svg', commented_at: '2달 전', body: '영상 잘 봤습니다', liked: 1, disliked: 0},
+    {author: 'test', profile: '../../../images/marcus.svg', commented_at: '24일 전', body: '댓글 내용 테스트중', liked: 30, disliked: 0},
+    {author: 'oliver', profile: '../../../images/marcus.svg', commented_at: '2시간 전', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio. Etiam vel consectetur lacus, vel efficitur sem. Suspendisse vestibulum libero sodales velit facilisis, at maximus ligula dignissim. Cras dui augue, dignissim a ullamcorper ut, condimentum eu enim. Quisque eleifend efficitur feugiat. Integer consequat vestibulum risus, sed ornare mi tincidunt non. Nunc malesuada rhoncus diam, vel cursus odio euismod nec. Donec ac laoreet lectus.', liked: 100, disliked: 10},
+    {author: 'loopin', profile: '../../../images/marcus.svg', commented_at: '30분 전', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar volutpat vehicula. Donec efficitur lacus lectus, blandit aliquet justo euismod ut. Sed justo ex, condimentum non rutrum et, lacinia at odio.', liked: 100, disliked: 1},
 ]
 
 // 이미지 경로
@@ -14,7 +16,8 @@ const public_url = '../../../images/';
 const temp_div = document.createElement("div");
 
 // 템플릿 코드를 사용하여 비디오 컨텐츠 생성
-fetch("../components/videoComponents/html/commentTemplate.html")
+function commentInsert() {
+    fetch("../components/videoComponents/html/commentTemplate.html")
     .then(res => {
         if (!res.ok) {
             throw new Error("HTML template 불러오기 실패");
@@ -41,5 +44,30 @@ fetch("../components/videoComponents/html/commentTemplate.html")
             clone.querySelector(".comment-body").textContent = el.body;
             clone.querySelector(".comment-liked-number").textContent = el.liked;
             contents.appendChild(clone);
-        })
+        });
     })
+}
+
+commentInsert();
+
+//내가 쓴 댓글 추가
+const my_comment = document.querySelector("#comment-form");
+
+my_comment.addEventListener("submit", function commentBtn(e) {
+    e.preventDefault();
+    
+    const mycomments = {};
+    mycomments.author = document.querySelector("#my-name").textContent;
+    mycomments.profile = document.querySelector("#my-profile").src;
+    mycomments.commented_at = timeCalculator(new Date());
+    mycomments.body = my_comment.querySelector("#comment").value;
+    mycomments.liked = 0;
+    mycomments.disliked = 0;
+
+    console.log(mycomments);
+
+    comments.push(mycomments);
+
+    commentInsert();
+    my_comment.querySelector("#comment").value = "";
+});
